780fadbbe145d4379ca96e902862acfd72294dd7
svitlyi <svitlyi_a@itstep.academy>
2024-12-01 13:46:55 +0200

    Added age-check feature

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ print("Hello world!")
 
 name = input("Як тебе звуть? ")
 print(f"Привіт, {name}!")
+
+age = int(input("Скільки тобі років? "))
+if age < 18:
+    print("Ти неповнолітній.")
+else:
+    print("Ти повнолітній.")
