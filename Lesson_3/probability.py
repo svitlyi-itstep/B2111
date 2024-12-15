3d649fed22e552e70c6df084f98113336b57744b
svitlyi <svitlyi_a@itstep.academy>
2024-12-15 14:56:59 +0200

    Lesson 3 code

--- Lesson_3/probability.py
+++ Lesson_3/probability.py
@@ -1,7 +1,7 @@
 import random
 
 def roll(probability):
-    return random.randint(1, 100) < probability
+    return random.randint(1, 100) <= probability
 
 # if виконається з верогідністю 30%
 if roll(30):
